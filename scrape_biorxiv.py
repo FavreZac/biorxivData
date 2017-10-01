73d6afed07bb2381aec4a477a4b59d77f165fdb5
timoast <timstuart90@gmail.com>
2017-10-01 21:33:51 +0800

    Can resume script

--- scrape_biorxiv.py
+++ scrape_biorxiv.py
@@ -6,7 +6,13 @@ from bs4 import BeautifulSoup
 import requests
 from time import sleep
 from datetime import datetime
+from argparse import ArgumentParser
 
+
+parser = ArgumentParser(description='scrape biorxiv site for view data')
+parser.add_argument('--pages', help='file containing paper titles and links', required=False)
+parser.add_argument('--data', help='file containing link data (will resume from last paper in file)', required=False)
+options = parser.parse_args()
 
 def make_soup(url):
     r = requests.get(url)
@@ -46,6 +52,13 @@ def get_paper_links(url):
                                   "link": "http://biorxiv.org" + i.a["href"]})
     return links
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 def get_metrics(url):
     """
     get number of abstract view and PDF downloads
@@ -74,11 +87,19 @@ def get_metrics(url):
     if len(views) >= 3:  # should be at least 3 fields, otherwise too new for metrics
         abstract = 0
         pdf = 0
-        for i in range(len(views)):
-            if i % 3 == 1:
-                abstract += int(views[i].text)
-            elif i % 3 == 2:
-                pdf += int(views[i].text)
+        # not all papers have a pdf version, some just have abstract views
+        # can check by looking at 3rd position
+        if is_number(views[2].text):
+            for i in range(len(views)):
+                if i % 3 == 1:
+                    abstract += int(views[i].text)
+                elif i % 3 == 2:
+                    pdf += int(views[i].text)
+        else:
+            pdf = "NA"
+            for i in range(len(views)):
+                if i % 2 == 1:
+                    abstract += int(views[i].text)
         abstract = str(abstract)
         pdf = str(pdf)
     else:
@@ -113,38 +134,82 @@ def get_metrics(url):
 
 
 url = "http://biorxiv.org/content/early/recent"
-pages = find_pages(url)
-all_papers = []
-
-for page in pages:
-    try:
-        get_paper_links(page)
-    except:
-        sleep(5)
-    else:
-        pass
-    all_papers += get_paper_links(page)
-    sleep(1)
-
 now = datetime.now()
 date = str(now.year) + "_" + str(now.month) + "_" + str(now.day)
 
-with open("biorxiv_data_" + date + ".tsv", "w+") as outfile:
-    outfile.write("Title\tURL\tAbstract views\tPDF views\tOriginal submission\tCurrent submission\n")
+if options.pages is None:
+    pages = find_pages(url)
+    all_papers = []
 
-    for i in all_papers:
+    for page in pages:
         try:
-            get_metrics(i["link"])
+            get_paper_links(page)
         except:
             sleep(5)
         else:
             pass
-        abstract, pdf, current, oldest = get_metrics(i["link"])
-        s = i['title'] + "\t" + \
-            i['link'] + "\t" + \
-            abstract + "\t" + \
-            pdf + "\t" + \
-            oldest + "\t" + \
-            current + "\n"
-        outfile.write(s.encode("UTF-8"))
+        all_papers += get_paper_links(page)
         sleep(1)
+
+    # save list of papers and links
+    with open("links_" + date + ".tsv", "w+") as outfile:
+        for i in all_papers:
+            s = i['link'] + '\t' + i['title'] + '\n'
+            outfile.write(s.encode("UTF-8"))
+else:
+    # read links from file
+    with open(options.pages, 'r') as infile:
+        all_papers = []
+        for line in infile:
+            line = line.strip('\n').split('\t')
+            all_papers.append({'link': line[0], 'title': line[1]})
+
+
+if options.data is None:
+    with open("biorxiv_data_" + date + ".tsv", "w+") as outfile:
+        outfile.write("Title\tURL\tAbstract views\tPDF views\tOriginal submission\tCurrent submission\n")
+
+        for i in all_papers:
+            try:
+                get_metrics(i["link"])
+            except:
+                sleep(5)
+            else:
+                pass
+            abstract, pdf, current, oldest = get_metrics(i["link"])
+            s = i['title'] + "\t" + \
+                i['link'] + "\t" + \
+                abstract + "\t" + \
+                pdf + "\t" + \
+                oldest + "\t" + \
+                current + "\n"
+            outfile.write(s.encode("UTF-8"))
+            sleep(1)
+else:
+    # get the last line so we know where to start from
+    with open(options.data, 'r') as infile:
+        for l in infile:
+            l = l.split('\t')
+    last_entry = {'link': l[1], 'title': l[0]}
+
+    # remove all links prior to last paper from all_papers
+    last_paper = all_papers.index(last_entry)
+    all_papers = all_papers[last_paper:]
+
+    with open(options.data, 'a') as outfile:
+        for i in all_papers:
+            try:
+                get_metrics(i["link"])
+            except:
+                sleep(5)
+            else:
+                pass
+            abstract, pdf, current, oldest = get_metrics(i["link"])
+            s = i['title'] + "\t" + \
+                i['link'] + "\t" + \
+                abstract + "\t" + \
+                pdf + "\t" + \
+                oldest + "\t" + \
+                current + "\n"
+            outfile.write(s)
+            sleep(1)
